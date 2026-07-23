4464b386b09a4f5c335445918830cbb6db2fab6b
Lukáš Hozda <luk.hozda@gmail.com>
2026-07-23 18:39:31 +0200

    Force local sources in repository checks

--- script/check.lisp
+++ script/check.lisp
@@ -1,14 +1,40 @@
 (require '#:asdf)
 
+(defun require-local-system-definition (name expected-source root)
+  "Require ASDF system NAME to come from EXPECTED-SOURCE below ROOT."
+  (let ((source
+          (asdf:system-source-file
+           (asdf:find-system name))))
+    (unless (and source
+                 (equal (truename source)
+                        (truename expected-source))
+                 (uiop:subpathp source root))
+      (error "ASDF resolved ~A to ~A instead of local source ~A."
+             name source expected-source))))
+
 (let* ((script (truename *load-truename*))
        (script-directory (uiop:pathname-directory-pathname script))
        (root (uiop:pathname-parent-directory-pathname script-directory))
+       (system-definition (merge-pathnames "mcparen.asd" root))
        (quicklisp (merge-pathnames "quicklisp/setup.lisp"
                                    (user-homedir-pathname))))
   (when (uiop:file-exists-p quicklisp)
     (load quicklisp :verbose nil :print nil))
-  (asdf:load-asd (merge-pathnames "mcparen.asd" root))
-  (if (find-package '#:ql)
-      (uiop:symbol-call '#:ql '#:quickload :mcparen/tests :silent t)
-      (asdf:load-system :mcparen/tests))
+  (asdf:clear-system '#:mcparen/tests)
+  (asdf:clear-system '#:mcparen)
+  (asdf:load-asd system-definition)
+  (require-local-system-definition '#:mcparen system-definition root)
+  (require-local-system-definition '#:mcparen/tests system-definition root)
+  (let ((warnings nil))
+    (handler-bind
+        ((warning
+           (lambda (condition)
+             (unless (typep condition 'style-warning)
+               (push (princ-to-string condition) warnings)))))
+      (asdf:load-system
+       '#:mcparen/tests
+       :force '(#:mcparen #:mcparen/tests)))
+    (when warnings
+      (error "Local Mcparen compilation emitted warnings:~%~{  ~A~%~}"
+             (nreverse warnings))))
   (asdf:test-system :mcparen))
